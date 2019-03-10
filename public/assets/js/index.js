e04941884c507ca598c41bd243897b9e83c4afbe
Kevin Castro <kevincastro@Katherines-MacBook-Pro.local>
2019-03-10 00:29:07 -0800

    change to vanilla js

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -1,4 +1,9 @@
-document.addEventListener(function () {
-  document.getElementsByClassName('parallax').parallax();
-  document.getElementsByClassName('sidenav').sidenav();
+document.addEventListener('DOMContentLoaded', function () {
+  // parallax
+  const parallaxElems = document.querySelectorAll('.parallax');
+  const parallaxiInstances = M.Parallax.init(parallaxElems, {});
+
+  // sidenav
+  var sidenav = document.querySelectorAll('.sidenav');
+  var instance = M.Sidenav.init(sidenav, {});
 });
